6c749c910d86f8d3f6accf87370ca21ba9037622
imseongtae <dev.haemil@gmail.com>
2021-02-19 18:46:06 +0900

    chore: Update eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,20 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+			'error',
+			// 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
+			// https://prettier.io/docs/en/options.html
+			{
+				semi: true,
+				singleQuote: true,
+				useTabs: false,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+			},
+		],
   },
 };
