95c4a25be5aedb89c94d877455e9abe9edc75f79
imseongtae <dev.haemil@gmail.com>
2021-04-16 01:19:47 +0900

    feat: Update movies controller to include crud routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  // Put,
+} from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  // 무언가 필요하면 직접 요청해야 함(데코레이터를 통해)
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    // 변수는 바꿀 수 있음
+    return `This will return one movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a movie';
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  // Put은 모든 리소스를 업데이트, Patch는 리소스의 일부분만 업데이트
+  // @Put('/:id')
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return `This will patch a movie with the id: ${movieId}`;
+  }
+}
