bece9185cc9200d8a31ed5d2178c7d977b98aacf
imseongtae <dev.haemil@gmail.com>
2021-07-18 23:57:24 +0900

    refactor: Modify movies controller to include search api

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,10 +1,12 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
   Param,
   Patch,
   Post,
+  Query,
   // Put,
 } from '@nestjs/common';
 
@@ -15,6 +17,12 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
+  // 파라미터에 전달하는 인자 id로 인식할 수 있으므로, @Get('/:id') 보다 위에 적는다.
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+
   // 무언가 필요하면 직접 요청해야 함(데코레이터를 통해)
   @Get('/:id')
   getOne(@Param('id') movieId: string) {
@@ -23,8 +31,20 @@ export class MoviesController {
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    console.log(movieData);
+    // return 'This will create a movie';
+    return movieData;
+  }
+
+  @Post('existing-user')
+  existingUser(@Body() userData) {
+    console.log(userData);
+    const res = {
+      token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJzdHJpbmciLCJuYW1lIjoi7J207IOB66-8IiwicGhvbmUiOiIrODIgMTAtNDQ1Ni03OTI4IiwiZ21haWwiOiJnZ3VnZUBtYWlsLmNvbSIsImlhdCI6MTYyMTQ0NjE4NiwiZXhwIjoxNjI0MDM4MTg2fQ.My_a992YPzGhisADzmfVozxpFJ7PVmZ5rNoQGSbo2i0',
+    };
+    return res;
   }
 
   @Delete('/:id')
